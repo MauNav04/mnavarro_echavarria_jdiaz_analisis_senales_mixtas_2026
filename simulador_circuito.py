ee27d5a42872cd38ea65e2742027974ee25f672f
JafetDM <148803642+JafetDM@users.noreply.github.com>
2026-03-06 00:09:47 -0600

    Funcionalidad: pedir_datos funciona

--- simulador_circuito.py
+++ simulador_circuito.py
@@ -12,6 +12,32 @@ import matplotlib.pyplot as plt
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Función para solicitar datos: VS, VR, VZload, R 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+def pedir_datos():
+
+    n = 0
+
+    while n == 0:
+        v_s = float(input("Ingresa el voltaje de la fuente VS: "))
+        v_r = float(input("Ingresa el voltaje de la resistencia VR: "))
+        v_z_load = float(input("Ingresa el voltaje de la carga inductiva VZLoad: "))
+        r = float(input("Ingresa el valor de la resistencia R: "))
+
+        # Validar que NO OCURRA VS > VR + VZLoad 
+
+        aux = v_r + v_z_load
+
+        if v_s > aux:
+            print("\nPor favor ingrese datos que NO SEAN VS > VR + VZLoad \n")
+            n = 0
+        
+        else:
+            print("\nDatos aceptados \n")
+            n = 1
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Función para calcular impedancia
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -20,16 +46,4 @@ import matplotlib.pyplot as plt
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 if __name__ == '__main__':
-    print("Prueba")
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Solicitar datos: VS, VR, VZload, R 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Validar VS > VR + VZLoad 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-
+    pedir_datos()
