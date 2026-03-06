a9740e3e0c05f9efa9590ad10909e506ffd532fa
JafetDM <148803642+JafetDM@users.noreply.github.com>
2026-03-05 23:55:18 -0600

    Documentacion: se agrega plantilla

---
+++ simulador_circuito.py
@@ -0,0 +1,35 @@
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Imports
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Sesión asincrónica - Semana 2
+# CE-1110 Análisis de Señales Mixtas
+# Jafet - Emanuel - Mauro
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Función para calcular impedancia
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Programa Principal
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+if __name__ == '__main__':
+    print("Prueba")
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Solicitar datos: VS, VR, VZload, R 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Validar VS > VR + VZLoad 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
